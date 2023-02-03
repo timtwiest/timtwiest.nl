51493e45962bdfaacaecd5e8c298510b9b7ef738
Tim Twiest <hello@timtwiest.nl>
2023-02-03 22:32:07 +0100

    tune the main-page, add cards, and a picture of me

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,20 +1,51 @@
 'use client';
 
 import React from 'react';
+import Image from 'next/image';
+import { GithubIcon } from '@components/icons';
+import LinkedinIcon from '@components/icons/linkedin-icon';
 
 export default function HomePage() {
   return (
-    <div className="relative z-0 flex min-h-screen flex-col overflow-hidden bg-gradient-to-br from-blue-200 via-transparent to-transparent">
-      <div className="relative px-6 lg:px-8">
-        <div className="mx-auto max-w-3xl space-y-5 pt-12 pb-12">
-          <h1 className="text-[26px] font-bold leading-[32px] text-black sm:text-3xl md:text-5xl">
-            Tim Twiest
-          </h1>
-          <span className="text-base font-medium text-gray-500 md:text-lg lg:text-xl">
-            <span className="text-gray-900">Full-stack</span> Developer @
-            PanCompany
-          </span>
+    <div className="relative z-0 flex min-h-screen flex-col overflow-hidden bg-gradient-to-br from-zinc-200 via-transparent to-transparent">
+      <div className="mx-auto grid max-w-3xl grid-cols-6 gap-6 px-6 pb-40 pt-16">
+        <div className="p-200 col-span-4 flex items-center justify-center overflow-hidden rounded-2xl bg-pink-200 md:col-span-4 md:h-52">
+          <div className="flex flex-col items-center space-y-4 py-8 px-6 md:flex-row md:space-y-0 md:space-x-4">
+            <Image
+              src={'/me.jpeg'}
+              alt={'Tim Twiest'}
+              width={96}
+              height={96}
+              className={
+                'h-24 w-24 rounded-full border border-pink-500 object-cover'
+              }
+            />
+            <div className="space-y-1">
+              <h1 className="font-title text-center text-xl font-bold tracking-tighter text-pink-900 md:text-left">
+                Tim Twiest
+              </h1>
+              <span className="text-center text-pink-800 md:text-left">
+                Full-stack Developer at{' '}
+                <a
+                  className="hover:border-b-2 hover:border-dotted hover:border-pink-900"
+                  href={'https://www.pancompany.com/'}
+                >
+                  PanCompany &#8599;
+                </a>
+              </span>
+            </div>
+          </div>
         </div>
+        <a
+          rel="noopener noreferrer"
+          target="_blank"
+          href="https://github.com/timtwiest"
+          className="group col-span-2 h-full"
+        >
+          <div className="flex h-full transform-gpu items-center justify-center rounded-2xl bg-zinc-900 text-4xl text-white transition-all duration-500 will-change-[outline,_transform] active:scale-100 group-hover:scale-95">
+            <GithubIcon />
+          </div>
+        </a>
       </div>
     </div>
   );
---
+++ src/components/icons/index.tsx
@@ -0,0 +1,2 @@
+export { default as GithubIcon } from './github-icon';
+export { default as Linkedin } from './linkedin-icon';
