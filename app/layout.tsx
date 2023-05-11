49eb2ed6b67883ec1e9b0e619dc40f5331cfcf43
Tim Twiest <hello@timtwiest.nl>
2023-05-11 22:07:10 +0200

    feat: move header to its own component named site-header.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -7,6 +7,7 @@ import { ModeToggle } from '@/components/mode-toggle';
 import { Metadata } from 'next';
 import { siteConfig } from '@/config/site';
 import { ReactNode } from 'react';
+import { SiteHeader } from '@/components/site-header';
 
 const inter = Inter({ subsets: ['latin'] });
 
@@ -39,33 +40,7 @@ export default function RootLayout({ children }: RootLayoutProps) {
       >
         <ThemeProvider attribute="class" defaultTheme="system" enableSystem>
           <div className="mx-auto max-w-2xl px-4 py-8">
-            <header className="container">
-              <div className="flex items-center justify-between border-b py-4">
-                <div className="flex items-center space-x-2">
-                  <div className="flex flex-col space-y-1 text-sm leading-none">
-                    <Link href="/" className="flex flex-col">
-                      <span className="text-lg font-bold">Tim Twiest</span>
-                      <span>Turns out I create, break, and fix things.</span>
-                    </Link>
-                    <div className="pt-1">
-                      <p className="text-muted-foreground">
-                        I&apos;m a full-stack developer, currently working at{' '}
-                        <Link
-                          href="https://pancompany.com"
-                          className="font-medium hover:underline hover:underline-offset-4"
-                          target="_blank"
-                        >
-                          PanCompany &#8599;
-                        </Link>
-                      </p>
-                    </div>
-                  </div>
-                </div>
-                <div className="flex items-center space-x-2">
-                  <ModeToggle />
-                </div>
-              </div>
-            </header>
+            <SiteHeader />
             <main>{children}</main>
           </div>
           <Analytics />
