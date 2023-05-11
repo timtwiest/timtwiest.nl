66a4c1effe0beaf433d900519aae2177c1c57acc
Tim Twiest <hello@timtwiest.nl>
2023-05-11 21:51:50 +0200

    fix: update div to section html tag for section-item.tsx

--- components/section-item.tsx
+++ components/section-item.tsx
@@ -10,10 +10,10 @@ interface IProps {
 
 export function SectionItem({ children, title, description }: IProps) {
   return (
-    <div className="space-y-1 pt-6">
+    <section className="space-y-1 pt-6">
       <h2 className="font-heading text-xl">{title}</h2>
       <p className="text-muted-foreground">{description}</p>
       {children}
-    </div>
+    </section>
   );
 }
